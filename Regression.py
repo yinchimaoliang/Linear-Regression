37c6b0b2d1c02ffbe63aadc8eb77f99f838a80a2
Yinhao Li <liyinhao0413@qq.com>
2019-03-06 20:08:41 +0800

    finish Regression

--- Regression.py
+++ Regression.py
@@ -2,27 +2,30 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
+import torch.nn.functional as F
 
 EPOCH = 1000
 
 class Net(nn.Module):
     def __init__(self):
         super(Net,self).__init__()
-        self.Linear = nn.Linear(1,1)
+        self.hidden = nn.Linear(1,100)
+        self.predict = nn.Linear(100,1)
     def forward(self, x):
-        out = self.Linear(x)
+        out = F.relu(self.hidden(x)) #非常重要，去线性化的relu函数，否则永远是一条直线
+        out = self.predict(out)
         return out
 
 class Main():
     def getData(self):
         self.x = torch.unsqueeze(torch.linspace(-1,1,100),dim = 1)#增加一维，变为二维
         # self.x = torch.linspace(-1,1,100)
-        self.y = self.x * 3 + 1 +  torch.rand(self.x.size())#添加噪点
+        self.y = self.x.pow(3) + 1 +  0.3 * torch.rand(self.x.size())#添加噪点
         # plt.scatter(self.x.data.numpy(),self.y.data.numpy())
         # plt.show()
     def main(self):
         model = Net().cuda()
-        optimizer = torch.optim.SGD(model.parameters(),lr = 0.02)
+        optimizer = torch.optim.SGD(model.parameters(),lr = 0.2)
         loss_func = nn.MSELoss()
         plt.ion()
         for epoch in range(EPOCH):
